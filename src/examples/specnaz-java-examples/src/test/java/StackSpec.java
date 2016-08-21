eefcbab8fb4b61a91275e5c767d51f4c4feba623
Adam Ruka <adamruka85@gmail.com>
2016-08-21 17:47:41 +0200

    Added a Stack example in Java.

---
+++ src/examples/specnaz-java-examples/src/test/java/StackSpec.java
@@ -0,0 +1,33 @@
+import org.junit.Assert;
+import org.specnaz.junit.SpecnazJUnit;
+
+import java.util.Stack;
+
+public class StackSpec extends SpecnazJUnit {{
+    describes("A Stack", it -> {
+        Stack<Integer> stack = new Stack<>();
+
+        it.endsEach(() -> {
+            stack.clear();
+        });
+
+        it.should("be empty when first created", () -> {
+            Assert.assertTrue(stack.isEmpty());
+        });
+
+        it.describes("with 10 and 20 pushed on it", () -> {
+            it.beginsEach(() -> {
+                stack.push(10);
+                stack.push(20);
+            });
+
+            it.should("have size equal to 2", () -> {
+                Assert.assertEquals(2, stack.size());
+            });
+
+            it.should("have 20 as the top element", () -> {
+                Assert.assertEquals(20, stack.peek().intValue());
+            });
+        });
+    });
+}}
