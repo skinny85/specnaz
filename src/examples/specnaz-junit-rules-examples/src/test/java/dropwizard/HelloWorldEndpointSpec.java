ad49b970d16425b1af29aff8bd3a349f2a5d97d6
Adam Ruka <adamruka85@gmail.com>
2018-01-10 21:45:43 -0800

    Add a (passing!) Dropwizard spec.

---
+++ src/examples/specnaz-junit-rules-examples/src/test/java/dropwizard/HelloWorldEndpointSpec.java
@@ -0,0 +1,54 @@
+package dropwizard;
+
+import com.fasterxml.jackson.databind.ObjectReader;
+import io.dropwizard.testing.junit.ResourceTestRule;
+import org.specnaz.junit.SpecnazJUnit2_Rules;
+import org.specnaz.junit.rules.Rule;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloWorldEndpointSpec extends SpecnazJUnit2_Rules {
+    public final Rule<ResourceTestRule> resources = Rule.of(() -> ResourceTestRule.builder()
+            .addResource(new HelloWorldResource("Hello, %s!", "Stranger"))
+            .build());
+
+    {
+        describes("The HelloWorld endpoint", it -> {
+            it.should("greet 'dropwizard' correctly", () -> {
+                // Hit the endpoint and get the raw json string
+                String resp = resources.r().client()
+                        .target("/hello-world")
+                        .queryParam("name", "dropwizard")
+                        .request()
+                        .get(String.class);
+
+                // The expected json that will be returned
+                String json = "{ \"id\": 1, \"content\": \"Hello, dropwizard!\" }";
+
+                // The object responsible for translating json to our class
+                ObjectReader reader = resources.r().getObjectMapper().readerFor(Saying.class);
+
+                // Deserialize our actual and expected responses
+                Saying actual = reader.readValue(resp);
+                Saying expected = reader.readValue(json);
+
+                assertThat(actual.getId())
+                        .isEqualTo(expected.getId())
+                        .isEqualTo(1);
+
+                assertThat(actual.getContent())
+                        .isEqualTo(expected.getContent())
+                        .isEqualTo("Hello, dropwizard!");
+            });
+
+            it.should("default the greeting to 'Stranger'", () -> {
+                // A more terse way to test just an endpoint
+                Saying actual = resources.r().client().target("/hello-world")
+                        .request().get(Saying.class);
+
+                assertThat(actual.getId()).isEqualTo(1);
+                assertThat(actual.getContent()).isEqualTo("Hello, Stranger!");
+            });
+        });
+    }
+}
