1f124332babc746e7afc5e4d0c4deea192e3be52
Adam Ruka <adamruka85@gmail.com>
2018-01-11 20:52:02 -0800

    Add a test using Spring's @IfProfileValue annotation.

--- src/examples/specnaz-junit-rules-examples/src/test/java/SpringIntegrationTest.java
+++ src/examples/specnaz-junit-rules-examples/src/test/java/SpringIntegrationTest.java
@@ -2,11 +2,13 @@ import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.IfProfileValue;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.rules.SpringClassRule;
 import org.springframework.test.context.junit4.rules.SpringMethodRule;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 @ContextConfiguration(classes = TestSpringConfig.class)
 public class SpringIntegrationTest {
@@ -22,5 +24,11 @@ public class SpringIntegrationTest {
     @Test
     public void test_method() {
         assertThat(serviceA.findA()).isEqualTo("ServiceA:TestDaoA");
+    }
+
+    @Test
+    @IfProfileValue(name="test_prop")
+    public void test_if_profile_value() throws Exception {
+        fail("this should not be executed");
     }
 }
