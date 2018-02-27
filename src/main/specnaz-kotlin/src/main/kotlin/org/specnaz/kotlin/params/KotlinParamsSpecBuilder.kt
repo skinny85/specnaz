2e60f389de91d258ed5a245f788be460ce79e394
Adam Ruka <adamruka85@gmail.com>
2018-02-26 23:17:32 -0800

    Add parametrized should(Throw) 7 through 9 and describes 7 through 9 to the Kotlin interface.

--- src/main/specnaz-kotlin/src/main/kotlin/org/specnaz/kotlin/params/KotlinParamsSpecBuilder.kt
+++ src/main/specnaz-kotlin/src/main/kotlin/org/specnaz/kotlin/params/KotlinParamsSpecBuilder.kt
@@ -7,18 +7,27 @@ import org.specnaz.params.ParamsExpected3
 import org.specnaz.params.ParamsExpected4
 import org.specnaz.params.ParamsExpected5
 import org.specnaz.params.ParamsExpected6
+import org.specnaz.params.ParamsExpected7
+import org.specnaz.params.ParamsExpected8
+import org.specnaz.params.ParamsExpected9
 import org.specnaz.params.ParamsExpectedException1
 import org.specnaz.params.ParamsExpectedException2
 import org.specnaz.params.ParamsExpectedException3
 import org.specnaz.params.ParamsExpectedException4
 import org.specnaz.params.ParamsExpectedException5
 import org.specnaz.params.ParamsExpectedException6
+import org.specnaz.params.ParamsExpectedException7
+import org.specnaz.params.ParamsExpectedException8
+import org.specnaz.params.ParamsExpectedException9
 import org.specnaz.params.ParamsExpectedSubgroup1
 import org.specnaz.params.ParamsExpectedSubgroup2
 import org.specnaz.params.ParamsExpectedSubgroup3
 import org.specnaz.params.ParamsExpectedSubgroup4
 import org.specnaz.params.ParamsExpectedSubgroup5
 import org.specnaz.params.ParamsExpectedSubgroup6
+import org.specnaz.params.ParamsExpectedSubgroup7
+import org.specnaz.params.ParamsExpectedSubgroup8
+import org.specnaz.params.ParamsExpectedSubgroup9
 import org.specnaz.params.ParamsSpecBuilder
 
 /**
@@ -392,6 +401,207 @@ class KotlinParamsSpecBuilder(val paramsSpecBuilder: ParamsSpecBuilder) : Kotlin
     }
 
     /**
+     * The parametrized equivalent of [org.specnaz.SpecBuilder.should],
+     * taking 7 parameters, for Kotlin.
+     */
+    fun <P1, P2, P3, P4, P5, P6, P7> should(description: String, testBody: (P1, P2, P3, P4, P5, P6, P7) -> Unit):
+            ParamsExpected7<P1, P2, P3, P4, P5, P6, P7> {
+        return paramsSpecBuilder.should(description, testBody)
+    }
+
+    /**
+     * The parametrized equivalent of [org.specnaz.SpecBuilder.fshould],
+     * taking 7 parameters, for Kotlin.
+     */
+    @Deprecated("Deprecated for the same reason SpecBuilder#fshould is")
+    fun <P1, P2, P3, P4, P5, P6, P7> fshould(description: String, testBody: (P1, P2, P3, P4, P5, P6, P7) -> Unit):
+            ParamsExpected7<P1, P2, P3, P4, P5, P6, P7> {
+        @Suppress("DEPRECATION")
+        return paramsSpecBuilder.fshould(description, testBody)
+    }
+
+    /**
+     * The parametrized equivalent of [org.specnaz.SpecBuilder.xshould],
+     * taking 7 parameters, for Kotlin.
+     */
+    fun <P1, P2, P3, P4, P5, P6, P7> xshould(description: String, testBody: (P1, P2, P3, P4, P5, P6, P7) -> Unit):
+            ParamsExpected7<P1, P2, P3, P4, P5, P6, P7> {
+        return paramsSpecBuilder.xshould(description, testBody)
+    }
+
+    /**
+     * The parametrized equivalent of [org.specnaz.SpecBuilder.shouldThrow],
+     * taking 7 parameters, for Kotlin.
+     */
+    inline fun <reified T : Throwable, P1, P2, P3, P4, P5, P6, P7> shouldThrow(
+            description: String, crossinline testBody: (P1, P2, P3, P4, P5, P6, P7) -> Unit):
+            ParamsExpectedException7<T, P1, P2, P3, P4, P5, P6, P7> {
+        return paramsSpecBuilder.shouldThrow(T::class.java, description, {
+            p1, p2, p3, p4, p5, p6, p7 -> testBody.invoke(p1, p2, p3, p4, p5, p6, p7)
+        })
+    }
+
+    /**
+     * The parametrized equivalent of [org.specnaz.SpecBuilder.fshouldThrow],
+     * taking 7 parameters, for Kotlin.
+     */
+    @Deprecated("Deprecated for the same reason SpecBuilder#fshould is")
+    inline fun <reified T : Throwable, P1, P2, P3, P4, P5, P6, P7> fshouldThrow(
+            description: String, crossinline testBody: (P1, P2, P3, P4, P5, P6, P7) -> Unit):
+            ParamsExpectedException7<T, P1, P2, P3, P4, P5, P6, P7> {
+        @Suppress("DEPRECATION")
+        return paramsSpecBuilder.fshouldThrow(T::class.java, description, {
+            p1, p2, p3, p4, p5, p6, p7 -> testBody.invoke(p1, p2, p3, p4, p5, p6, p7)
+        })
+    }
+
+    /**
+     * The parametrized equivalent of [org.specnaz.SpecBuilder.xshouldThrow],
+     * taking 7 parameters, for Kotlin.
+     */
+    inline fun <reified T : Throwable, P1, P2, P3, P4, P5, P6, P7> xshouldThrow(
+            description: String, crossinline testBody: (P1, P2, P3, P4, P5, P6, P7) -> Unit):
+            ParamsExpectedException7<T, P1, P2, P3, P4, P5, P6, P7> {
+        return paramsSpecBuilder.xshouldThrow(T::class.java, description, {
+            p1, p2, p3, p4, p5, p6, p7 -> testBody.invoke(p1, p2, p3, p4, p5, p6, p7)
+        })
+    }
+
+    /**
+     * The parametrized equivalent of [org.specnaz.SpecBuilder.should],
+     * taking 8 parameters, for Kotlin.
+     */
+    fun <P1, P2, P3, P4, P5, P6, P7, P8> should(description: String, testBody: (P1, P2, P3, P4, P5, P6, P7, P8) -> Unit):
+            ParamsExpected8<P1, P2, P3, P4, P5, P6, P7, P8> {
+        return paramsSpecBuilder.should(description, testBody)
+    }
+
+    /**
+     * The parametrized equivalent of [org.specnaz.SpecBuilder.fshould],
+     * taking 8 parameters, for Kotlin.
+     */
+    @Deprecated("Deprecated for the same reason SpecBuilder#fshould is")
+    fun <P1, P2, P3, P4, P5, P6, P7, P8> fshould(description: String, testBody: (P1, P2, P3, P4, P5, P6, P7, P8) -> Unit):
+            ParamsExpected8<P1, P2, P3, P4, P5, P6, P7, P8> {
+        @Suppress("DEPRECATION")
+        return paramsSpecBuilder.fshould(description, testBody)
+    }
+
+    /**
+     * The parametrized equivalent of [org.specnaz.SpecBuilder.xshould],
+     * taking 8 parameters, for Kotlin.
+     */
+    fun <P1, P2, P3, P4, P5, P6, P7, P8> xshould(description: String, testBody: (P1, P2, P3, P4, P5, P6, P7, P8) -> Unit):
+            ParamsExpected8<P1, P2, P3, P4, P5, P6, P7, P8> {
+        return paramsSpecBuilder.xshould(description, testBody)
+    }
+
+    /**
+     * The parametrized equivalent of [org.specnaz.SpecBuilder.shouldThrow],
+     * taking 8 parameters, for Kotlin.
+     */
+    inline fun <reified T : Throwable, P1, P2, P3, P4, P5, P6, P7, P8> shouldThrow(
+            description: String, crossinline testBody: (P1, P2, P3, P4, P5, P6, P7, P8) -> Unit):
+            ParamsExpectedException8<T, P1, P2, P3, P4, P5, P6, P7, P8> {
+        return paramsSpecBuilder.shouldThrow(T::class.java, description, {
+            p1, p2, p3, p4, p5, p6, p7, p8 -> testBody.invoke(p1, p2, p3, p4, p5, p6, p7, p8)
+        })
+    }
+
+    /**
+     * The parametrized equivalent of [org.specnaz.SpecBuilder.fshouldThrow],
+     * taking 8 parameters, for Kotlin.
+     */
+    @Deprecated("Deprecated for the same reason SpecBuilder#fshould is")
+    inline fun <reified T : Throwable, P1, P2, P3, P4, P5, P6, P7, P8> fshouldThrow(
+            description: String, crossinline testBody: (P1, P2, P3, P4, P5, P6, P7, P8) -> Unit):
+            ParamsExpectedException8<T, P1, P2, P3, P4, P5, P6, P7, P8> {
+        @Suppress("DEPRECATION")
+        return paramsSpecBuilder.fshouldThrow(T::class.java, description, {
+            p1, p2, p3, p4, p5, p6, p7, p8 -> testBody.invoke(p1, p2, p3, p4, p5, p6, p7, p8)
+        })
+    }
+
+    /**
+     * The parametrized equivalent of [org.specnaz.SpecBuilder.xshouldThrow],
+     * taking 8 parameters, for Kotlin.
+     */
+    inline fun <reified T : Throwable, P1, P2, P3, P4, P5, P6, P7, P8> xshouldThrow(
+            description: String, crossinline testBody: (P1, P2, P3, P4, P5, P6, P7, P8) -> Unit):
+            ParamsExpectedException8<T, P1, P2, P3, P4, P5, P6, P7, P8> {
+        return paramsSpecBuilder.xshouldThrow(T::class.java, description, {
+            p1, p2, p3, p4, p5, p6, p7, p8 -> testBody.invoke(p1, p2, p3, p4, p5, p6, p7, p8)
+        })
+    }
+
+    /**
+     * The parametrized equivalent of [org.specnaz.SpecBuilder.should],
+     * taking 9 parameters, for Kotlin.
+     */
+    fun <P1, P2, P3, P4, P5, P6, P7, P8, P9> should(description: String, testBody: (P1, P2, P3, P4, P5, P6, P7, P8, P9) -> Unit):
+            ParamsExpected9<P1, P2, P3, P4, P5, P6, P7, P8, P9> {
+        return paramsSpecBuilder.should(description, testBody)
+    }
+
+    /**
+     * The parametrized equivalent of [org.specnaz.SpecBuilder.fshould],
+     * taking 9 parameters, for Kotlin.
+     */
+    @Deprecated("Deprecated for the same reason SpecBuilder#fshould is")
+    fun <P1, P2, P3, P4, P5, P6, P7, P8, P9> fshould(description: String, testBody: (P1, P2, P3, P4, P5, P6, P7, P8, P9) -> Unit):
+            ParamsExpected9<P1, P2, P3, P4, P5, P6, P7, P8, P9> {
+        @Suppress("DEPRECATION")
+        return paramsSpecBuilder.fshould(description, testBody)
+    }
+
+    /**
+     * The parametrized equivalent of [org.specnaz.SpecBuilder.xshould],
+     * taking 9 parameters, for Kotlin.
+     */
+    fun <P1, P2, P3, P4, P5, P6, P7, P8, P9> xshould(description: String, testBody: (P1, P2, P3, P4, P5, P6, P7, P8, P9) -> Unit):
+            ParamsExpected9<P1, P2, P3, P4, P5, P6, P7, P8, P9> {
+        return paramsSpecBuilder.xshould(description, testBody)
+    }
+
+    /**
+     * The parametrized equivalent of [org.specnaz.SpecBuilder.shouldThrow],
+     * taking 9 parameters, for Kotlin.
+     */
+    inline fun <reified T : Throwable, P1, P2, P3, P4, P5, P6, P7, P8, P9> shouldThrow(
+            description: String, crossinline testBody: (P1, P2, P3, P4, P5, P6, P7, P8, P9) -> Unit):
+            ParamsExpectedException9<T, P1, P2, P3, P4, P5, P6, P7, P8, P9> {
+        return paramsSpecBuilder.shouldThrow(T::class.java, description, {
+            p1, p2, p3, p4, p5, p6, p7, p8, p9 -> testBody.invoke(p1, p2, p3, p4, p5, p6, p7, p8, p9)
+        })
+    }
+
+    /**
+     * The parametrized equivalent of [org.specnaz.SpecBuilder.fshouldThrow],
+     * taking 9 parameters, for Kotlin.
+     */
+    @Deprecated("Deprecated for the same reason SpecBuilder#fshould is")
+    inline fun <reified T : Throwable, P1, P2, P3, P4, P5, P6, P7, P8, P9> fshouldThrow(
+            description: String, crossinline testBody: (P1, P2, P3, P4, P5, P6, P7, P8, P9) -> Unit):
+            ParamsExpectedException9<T, P1, P2, P3, P4, P5, P6, P7, P8, P9> {
+        @Suppress("DEPRECATION")
+        return paramsSpecBuilder.fshouldThrow(T::class.java, description, {
+            p1, p2, p3, p4, p5, p6, p7, p8, p9 -> testBody.invoke(p1, p2, p3, p4, p5, p6, p7, p8, p9)
+        })
+    }
+
+    /**
+     * The parametrized equivalent of [org.specnaz.SpecBuilder.xshouldThrow],
+     * taking 9 parameters, for Kotlin.
+     */
+    inline fun <reified T : Throwable, P1, P2, P3, P4, P5, P6, P7, P8, P9> xshouldThrow(
+            description: String, crossinline testBody: (P1, P2, P3, P4, P5, P6, P7, P8, P9) -> Unit):
+            ParamsExpectedException9<T, P1, P2, P3, P4, P5, P6, P7, P8, P9> {
+        return paramsSpecBuilder.xshouldThrow(T::class.java, description, {
+            p1, p2, p3, p4, p5, p6, p7, p8, p9 -> testBody.invoke(p1, p2, p3, p4, p5, p6, p7, p8, p9)
+        })
+    }
+
+    /**
      * The parametrized equivalent of [org.specnaz.SpecBuilder.describes],
      * taking a single parameter, for Kotlin.
      */
@@ -530,7 +740,7 @@ class KotlinParamsSpecBuilder(val paramsSpecBuilder: ParamsSpecBuilder) : Kotlin
 
     /**
      * The parametrized equivalent of [org.specnaz.SpecBuilder.describes],
-     * taking 5 parameters, for Kotlin.
+     * taking 6 parameters, for Kotlin.
      */
     fun <P1, P2, P3, P4, P5, P6> describes(description: String, specClosure: (P1, P2, P3, P4, P5, P6) -> Unit):
             ParamsExpectedSubgroup6<P1, P2, P3, P4, P5, P6> {
@@ -539,7 +749,7 @@ class KotlinParamsSpecBuilder(val paramsSpecBuilder: ParamsSpecBuilder) : Kotlin
 
     /**
      * The parametrized equivalent of [org.specnaz.SpecBuilder.fdescribes],
-     * taking 5 parameters, for Kotlin.
+     * taking 6 parameters, for Kotlin.
      */
     @Deprecated("Deprecated for the same reason SpecBuilder#fshould is")
     fun <P1, P2, P3, P4, P5, P6> fdescribes(description: String, specClosure: (P1, P2, P3, P4, P5, P6) -> Unit):
@@ -550,10 +760,97 @@ class KotlinParamsSpecBuilder(val paramsSpecBuilder: ParamsSpecBuilder) : Kotlin
 
     /**
      * The parametrized equivalent of [org.specnaz.SpecBuilder.xdescribes],
-     * taking 5 parameters, for Kotlin.
+     * taking 6 parameters, for Kotlin.
      */
     fun <P1, P2, P3, P4, P5, P6> xdescribes(description: String, specClosure: (P1, P2, P3, P4, P5, P6) -> Unit):
             ParamsExpectedSubgroup6<P1, P2, P3, P4, P5, P6> {
+        return paramsSpecBuilder.xdescribes(description, specClosure)
+    }
+
+    /**
+     * The parametrized equivalent of [org.specnaz.SpecBuilder.describes],
+     * taking 7 parameters, for Kotlin.
+     */
+    fun <P1, P2, P3, P4, P5, P6, P7> describes(description: String, specClosure: (P1, P2, P3, P4, P5, P6, P7) -> Unit):
+            ParamsExpectedSubgroup7<P1, P2, P3, P4, P5, P6, P7> {
+        return paramsSpecBuilder.describes(description, specClosure)
+    }
+
+    /**
+     * The parametrized equivalent of [org.specnaz.SpecBuilder.fdescribes],
+     * taking 7 parameters, for Kotlin.
+     */
+    @Deprecated("Deprecated for the same reason SpecBuilder#fshould is")
+    fun <P1, P2, P3, P4, P5, P6, P7> fdescribes(description: String, specClosure: (P1, P2, P3, P4, P5, P6, P7) -> Unit):
+            ParamsExpectedSubgroup7<P1, P2, P3, P4, P5, P6, P7> {
+        @Suppress("DEPRECATION")
+        return paramsSpecBuilder.fdescribes(description, specClosure)
+    }
+
+    /**
+     * The parametrized equivalent of [org.specnaz.SpecBuilder.xdescribes],
+     * taking 7 parameters, for Kotlin.
+     */
+    fun <P1, P2, P3, P4, P5, P6, P7> xdescribes(description: String, specClosure: (P1, P2, P3, P4, P5, P6, P7) -> Unit):
+            ParamsExpectedSubgroup7<P1, P2, P3, P4, P5, P6, P7> {
+        return paramsSpecBuilder.xdescribes(description, specClosure)
+    }
+
+    /**
+     * The parametrized equivalent of [org.specnaz.SpecBuilder.describes],
+     * taking 8 parameters, for Kotlin.
+     */
+    fun <P1, P2, P3, P4, P5, P6, P7, P8> describes(description: String, specClosure: (P1, P2, P3, P4, P5, P6, P7, P8) -> Unit):
+            ParamsExpectedSubgroup8<P1, P2, P3, P4, P5, P6, P7, P8> {
+        return paramsSpecBuilder.describes(description, specClosure)
+    }
+
+    /**
+     * The parametrized equivalent of [org.specnaz.SpecBuilder.fdescribes],
+     * taking 8 parameters, for Kotlin.
+     */
+    @Deprecated("Deprecated for the same reason SpecBuilder#fshould is")
+    fun <P1, P2, P3, P4, P5, P6, P7, P8> fdescribes(description: String, specClosure: (P1, P2, P3, P4, P5, P6, P7, P8) -> Unit):
+            ParamsExpectedSubgroup8<P1, P2, P3, P4, P5, P6, P7, P8> {
+        @Suppress("DEPRECATION")
+        return paramsSpecBuilder.fdescribes(description, specClosure)
+    }
+
+    /**
+     * The parametrized equivalent of [org.specnaz.SpecBuilder.xdescribes],
+     * taking 8 parameters, for Kotlin.
+     */
+    fun <P1, P2, P3, P4, P5, P6, P7, P8> xdescribes(description: String, specClosure: (P1, P2, P3, P4, P5, P6, P7, P8) -> Unit):
+            ParamsExpectedSubgroup8<P1, P2, P3, P4, P5, P6, P7, P8> {
+        return paramsSpecBuilder.xdescribes(description, specClosure)
+    }
+
+    /**
+     * The parametrized equivalent of [org.specnaz.SpecBuilder.describes],
+     * taking 9 parameters, for Kotlin.
+     */
+    fun <P1, P2, P3, P4, P5, P6, P7, P8, P9> describes(description: String, specClosure: (P1, P2, P3, P4, P5, P6, P7, P8, P9) -> Unit):
+            ParamsExpectedSubgroup9<P1, P2, P3, P4, P5, P6, P7, P8, P9> {
+        return paramsSpecBuilder.describes(description, specClosure)
+    }
+
+    /**
+     * The parametrized equivalent of [org.specnaz.SpecBuilder.fdescribes],
+     * taking 9 parameters, for Kotlin.
+     */
+    @Deprecated("Deprecated for the same reason SpecBuilder#fshould is")
+    fun <P1, P2, P3, P4, P5, P6, P7, P8, P9> fdescribes(description: String, specClosure: (P1, P2, P3, P4, P5, P6, P7, P8, P9) -> Unit):
+            ParamsExpectedSubgroup9<P1, P2, P3, P4, P5, P6, P7, P8, P9> {
+        @Suppress("DEPRECATION")
+        return paramsSpecBuilder.fdescribes(description, specClosure)
+    }
+
+    /**
+     * The parametrized equivalent of [org.specnaz.SpecBuilder.xdescribes],
+     * taking 9 parameters, for Kotlin.
+     */
+    fun <P1, P2, P3, P4, P5, P6, P7, P8, P9> xdescribes(description: String, specClosure: (P1, P2, P3, P4, P5, P6, P7, P8, P9) -> Unit):
+            ParamsExpectedSubgroup9<P1, P2, P3, P4, P5, P6, P7, P8, P9> {
         return paramsSpecBuilder.xdescribes(description, specClosure)
     }
 }
