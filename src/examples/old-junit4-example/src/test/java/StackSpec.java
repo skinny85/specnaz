342fa460cc872ecfcb26abf2726d8a5008e000ee
Adam Ruka <adamruka85@gmail.com>
2018-03-10 16:02:14 -0800

    Example subproject using JUnit 4.11.

---
+++ src/examples/old-junit4-example/src/test/java/StackSpec.java
@@ -0,0 +1,35 @@
+import org.junit.Assert;
+import org.junit.Assume;
+import org.specnaz.junit.SpecnazJUnit;
+
+import java.util.Stack;
+
+public class StackSpec extends SpecnazJUnit {{
+    describes("A Stack", it -> {
+        Stack<Integer> stack = new Stack<>();
+
+        it.endsEach(() -> {
+            stack.clear();
+        });
+
+        it.should("be empty when first created", () -> {
+            Assume.assumeTrue(true);
+            Assert.assertTrue(stack.isEmpty());
+        });
+
+        it.describes("with 10 and 20 pushed on it", () -> {
+            it.beginsEach(() -> {
+                stack.push(10);
+                stack.push(20);
+            });
+
+            it.should("have size equal to 2", () -> {
+                Assert.assertEquals(2, stack.size());
+            });
+
+            it.should("have 20 as the top element", () -> {
+                Assert.assertEquals(20, (int)stack.peek());
+            });
+        });
+    });
+}}
