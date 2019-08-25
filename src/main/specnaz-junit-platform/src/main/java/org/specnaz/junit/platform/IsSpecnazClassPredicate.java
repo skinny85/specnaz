8a42bd13ad0bd7436fd09084b0897d6ea49c654e
Adam Ruka <adamruka85@gmail.com>
2019-08-25 10:40:21 -0700

    Exclude abstract classes (like interfaces!) from being recognized as Specnaz-compatible classes.

--- src/main/specnaz-junit-platform/src/main/java/org/specnaz/junit/platform/IsSpecnazClassPredicate.java
+++ src/main/specnaz-junit-platform/src/main/java/org/specnaz/junit/platform/IsSpecnazClassPredicate.java
@@ -3,6 +3,7 @@ package org.specnaz.junit.platform;
 import org.specnaz.Specnaz;
 import org.specnaz.params.SpecnazParams;
 
+import java.lang.reflect.Modifier;
 import java.util.function.Predicate;
 
 public enum IsSpecnazClassPredicate implements Predicate<Class<?>> {
@@ -10,7 +11,7 @@ public enum IsSpecnazClassPredicate implements Predicate<Class<?>> {
 
     @Override
     public boolean test(Class<?> classs) {
-        return Specnaz.class.isAssignableFrom(classs) ||
-                SpecnazParams.class.isAssignableFrom(classs);
+        return (Specnaz.class.isAssignableFrom(classs) ||
+                SpecnazParams.class.isAssignableFrom(classs)) && !Modifier.isAbstract(classs.getModifiers());
     }
 }
